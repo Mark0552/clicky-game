c506427d235aafdf8aa879dd80c89150c3793944
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2019-10-11 14:16:00 -0400

    working

---
+++ src/components/gameover.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const GameOver = (props) => {
+    if (props.score >= 12) {
+        return (
+            <div className="winner text-center container-fluid">
+                <button className="new-game btn" onClick={() => props.handleClick(props.gameover)}>new game</button>
+            </div>
+        )
+    } else {
+        return (
+            <div className="container text-center">
+                <h1 className="gameover">{props.countdown}</h1>
+            </div>
+        )
+    }
+}
+    ;
+
+export default GameOver;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="container">
+        <div className="jumbotron bg text-center">
+            <h1 className="logo">Clicky Game</h1>
+            <p className="desc">Click on a character to earn points, if you click the same character twice you lose!</p>
+        </div>
+    </div>
+);
+
+export default Header;
